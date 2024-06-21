d7a3df9c249892e4bd3b23bcd46bd52074697d4d
pavan591-0 <40891482+pavan591-0@users.noreply.github.com>
2024-06-22 00:16:26 +0530

    Updated test_calculator.py
    indentation corrected.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,23 +1,20 @@
 import unittest
-from src.calculator import add, subtract, divide 
+from src.calculator import add, subtract, divide
+
 class TestCalculator(unittest.TestCase):
-   def test_add(self):
-    self.assertEqual(add(2, 3), 5)
-    self.assertEqual(add(-1, 1), 0)
-    self.assertEqual(add(-1, -1), -2)
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 2), 1)
+        self.assertEqual(add(-1, -4), -5)
 
-  def test_subtract(self):
-    self.assertEqual(subtract(2, 3), -1)
-    self.assertEqual(subtract(-1, -1), 0)
-    self.assertEqual(subtract(-1, 4), -5)
+    def test_subtract(self):
+        self.assertEqual(subtract(4, 3), 1)
+        self.assertEqual(subtract(2, 1), 1)
+        self.assertEqual(subtract(-1, 1), -2)
 
-
-  def test_divide(self):
-    self.assertEqual(divide(2, 1), 2)
-    self.assertEqual(divide(10, 2), 5)
-    self.assertEqual(divide(-10, 1), -10)
-      
-      
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(-10, 2), -5)
 
 if __name__ == '__main__':
-   unittest.main()
+    unittest.main()
